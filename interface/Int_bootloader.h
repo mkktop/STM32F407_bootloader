7498a10405971b268a0be30c05356d25ce1d085b
mkk <2864078813@qq.com>
2026-02-16 23:12:26 +0800

    feat(bootloader): 实现flash写入和扇区擦除功能
    添加flash写入逻辑和扇区擦除功能，支持通过串口接收数据并写入指定地址
    更新.gitignore忽略构建目录和IDE配置文件

--- interface/Int_bootloader.h
+++ interface/Int_bootloader.h
@@ -3,7 +3,8 @@
 #include "usart.h"
 #include "string.h"
 #define BOOTLOADER_UART_BUF_SIZE 512 // 512字节的缓冲区
+#define APPLICATION_ADDRESS 0x08010000 // 应用程序A地址
 
 void Int_bootloader_init(void);
-
+uint32_t get_flash_sector(uint32_t address);
 #endif /* __INT_BOOTLOADER_H__ */
